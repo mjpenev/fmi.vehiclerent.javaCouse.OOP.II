c75bf7aca68f62ea2c855f638b73ab9ec2e12d96
Martin Penev <56087769+mjpenev@users.noreply.github.com>
2025-08-30 22:18:46 +0300

    Reworked RentalService class

--- vehiclerent/RentalService.java
+++ vehiclerent/RentalService.java
@@ -10,34 +10,14 @@ import java.time.LocalDateTime;
 
 public class RentalService {
 
-    public void rentVehicle(Driver driver, Vehicle vehicle, LocalDateTime startOfRent) throws VehicleAlreadyRentedException {
-        try {
-            vehicle.rent(driver, startOfRent);
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(String.format("Invalid argument. %s", e.getMessage()));
-        }
-        catch (VehicleAlreadyRentedException e) {
-            System.out.println(String.format("Vehicle is already rented. %s", e.getMessage()));
-        }
+    public void rentVehicle(Driver driver, Vehicle vehicle, LocalDateTime startOfRent)
+            throws IllegalArgumentException, VehicleAlreadyRentedException {
+        vehicle.rent(driver, startOfRent);
     }
 
-    public double returnVehicle(Vehicle vehicle, LocalDateTime endOfRent) throws IllegalArgumentException, InvalidRentingPeriodException, VehicleNotRentedException {
-        double price = 0;
-        try {
-            vehicle.returnBack(endOfRent);
-            price = vehicle.calculateRentalPrice(vehicle.getStartRentTime(), endOfRent);
-        }
-        catch (InvalidRentingPeriodException e) {
-            System.out.println("Period is not valid!");
-        }
-        catch (VehicleNotRentedException e) {
-            System.out.println("Vehicle is not rented!");
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println("There is an illegal argument!");
-        }
-
-        return price;
+    public double returnVehicle(Vehicle vehicle, LocalDateTime endOfRent)
+            throws IllegalArgumentException, InvalidRentingPeriodException, VehicleNotRentedException {
+        vehicle.returnBack(endOfRent);
+        return vehicle.calculateRentalPrice(vehicle.getStartRentTime(), endOfRent);
     }
 }
